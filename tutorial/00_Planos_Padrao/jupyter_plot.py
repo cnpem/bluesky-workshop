7aca65e1eba29cbe4346044980e753dd86f93235
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-09-04 10:03:01 -0300

    fix: make live plots (kinda) work on binder / inline
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

---
+++ tutorial/00_Planos_Padrao/jupyter_plot.py
@@ -0,0 +1,24 @@
+from bluesky.callbacks import LiveFitPlot, LivePlot
+
+def create_jupyter_live_plot(plot_cls):
+    import matplotlib.pyplot as plt
+    from IPython.display import display, clear_output
+
+    class __JupyterLivePlot(plot_cls):
+        def __init__(self, *args, fig=None, ax=None, **kwargs):
+            if fig is None:
+                fig, ax = plt.subplots()
+            super().__init__(*args, ax=ax, **kwargs)
+            self._figure = fig
+    
+        def __call__(self, event_name, event_data):
+            super().__call__(event_name, event_data)
+            display(self._figure, clear=True)
+            if event_name == "stop":
+                clear_output(True)
+            self._figure.canvas.draw()
+
+    return __JupyterLivePlot
+
+JupyterLivePlot = create_jupyter_live_plot(LivePlot)
+JupyterLiveFitPlot = create_jupyter_live_plot(LiveFitPlot)
